4a7adcd49b017390e602ad9d10133a7a55c9712b
Liviu Preda <liviupreda@gmail.com>
2019-10-01 13:10:26 +0300

    add readme.md, test spoonacular api, install axios

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Recipe Finder App
+
+- Recipe finder build using vanilla JS ES6 and Axios.
+
+- [Spoonacular API](https://spoonacular.com/food-api/docs) for recipes
+
+- In case of CORS related errors, you can use [this browser extension](https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en) for Google Chrome.
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 // Global app controller
-import mod1 from './test';
-const x = 23;
-console.log(`${mod1} imported from the module test.js Variable x: ${x}`);
+// food2fork.com API key 26541d5659322325bf74270a4bb31ace
+// GET https://www.food2fork.com/api/search
+// https://spoonacular.com/food-api/docs API key f46015c488304ae893fde89f7a4baf4d
+// GET https://api.spoonacular.com/recipes/search
+
+// Import Axios package for HTTP requests, JSON conversion and error handling
+import axios from 'axios';
+
+async function getResults(query, number) {
+  const key = 'f46015c488304ae893fde89f7a4baf4d';
+  const result = await axios(
+    `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}&number=${number}`
+  );
+  console.log(result);
+}
+getResults('cheese', 1);
