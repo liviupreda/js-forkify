93c51fa18666bf3fd62cd5331638fb58f4ed86ba
Liviu Preda <liviupreda@gmail.com>
2019-10-02 12:09:30 +0300

    shorten result titles to one row

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,16 +11,36 @@ export const clearRecipes = () => {
   elements.searchResultsUl.innerHTML = '';
 };
 
+// Limits the recipe title to one line, followed by '(...)'
+// default limit parameter is 15 characters (otherwise the three dots
+// are displayed on a second line in my browser at 100% size)
+const shortenTitle = (title, limit = 15) => {
+  const shortTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        shortTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0); // initial accumulator value is 0
+    // Return short title
+    return `${shortTitle.join(' ')} (...)`;
+  }
+  return title;
+};
+
 // Function for printing 1 recipe per call
 const renderRecipe = result => {
   const html = `
   <li>
     <a class="results__link" href="#${result.id}">
         <figure class="results__fig">
-            <img src="https://spoonacular.com/recipeImages/${result.id}-90x90" alt="${result.title}">
+            <img src="https://spoonacular.com/recipeImages/${
+              result.id
+            }-90x90" alt="${result.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${result.title}</h4>
+            <h4 class="results__name">${shortenTitle(result.title)}</h4>
             <p class="results__readyin">${result.readyInMinutes} minutes</p>
         </div>
     </a>
